8832a59d2b11ea820acd5f00c1e1e226e0755f40
REDLANTERNDEV <87618221+REDLANTERNDEV@users.noreply.github.com>
2025-02-18 13:03:13 +0300

    bump version to 0.1.0 and add styles for popup component

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,4 +1,16 @@
+import "./styles/popup.css";
 const Popup = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <button className="content">0.50</button>
+        <button className="content">1.0</button>
+        <button className="content">1.1</button>
+        <button className="content">1.5</button>
+        <button className="content">1.75</button>
+        <button className="content">2.5</button>
+      </div>
+    </div>
+  );
 };
 export default Popup;
